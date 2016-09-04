16f1ba8a600df9401bc78f2f73184218865f95a8
dma <dma@subgraph.com>
2016-09-04 03:30:20 +0000

    Updated godeps

--- vendor/github.com/subgraph/procsnitchd/client/client.go
+++ vendor/github.com/subgraph/procsnitchd/client/client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"net"
 	"net/rpc"
+	"time"
 
 	"github.com/op/go-logging"
 	"github.com/subgraph/go-procsnitch"
@@ -16,22 +17,55 @@ func SetLogger(logger *logging.Logger) {
 	log = logger
 }
 
+type Options struct {
+	// Max number of connect retries upon failure
+	MaxRetry   int
+	RetrySleep time.Duration
+}
+
+var defaultOptions = Options{
+	MaxRetry:   5,
+	RetrySleep: 300 * time.Millisecond,
+}
+
 type SnitchClient struct {
+	options    *Options
 	conn       net.Conn
 	client     *rpc.Client
 	socketFile string
 }
 
-func NewSnitchClient(socketFile string) *SnitchClient {
+// NewSnitchClient is used to talk to procsnitchd
+// options can set to nil in order to utilize defaults
+func NewSnitchClient(socketFile string, options *Options) *SnitchClient {
 	s := SnitchClient{
 		socketFile: socketFile,
+	}
+	if options == nil {
+		s.options = &defaultOptions
+	} else {
+		s.options = options
 	}
 	return &s
 }
 
 func (s *SnitchClient) Start() error {
+	return s.Dial()
+}
+
+func (s *SnitchClient) Dial() error {
 	var err error
-	s.conn, err = net.Dial("unix", s.socketFile)
+
+	// implement "retry" for net.Dial()
+	for i := 0; i < 5; i++ {
+		s.conn, err = net.Dial("unix", s.socketFile)
+		if err == nil {
+			break
+		}
+		log.Warningf("SnitchClient connect failure: %s. Retrying.", err)
+		// arbitrary "sleep" value
+		time.Sleep(300 * time.Millisecond)
+	}
 	if err != nil {
 		log.Errorf("SnitchClient Start aborted. Failed to connect: %s", err)
 		return err
@@ -52,7 +86,15 @@ func (s *SnitchClient) LookupUNIXSocketProcess(socketFile string) *procsnitch.In
 	info := procsnitch.Info{}
 	err = s.client.Call("ProcsnitchRPC.LookupUNIXSocketProcess", socketFile, &info)
 	if err != nil {
-		log.Error("LookupUNIXSocketProcess received a nil Info struct")
+		err = s.Dial()
+		if err != nil {
+			log.Errorf("LookupUNIXSocketProcess received a nil Info struct: %s", err)
+		} else {
+			err = s.client.Call("ProcsnitchRPC.LookupUNIXSocketProcess", socketFile, &info)
+			if err != nil {
+				log.Errorf("LookupUNIXSocketProcess received a nil Info struct: %s", err)
+			}
+		}
 	}
 	return &info
 }
@@ -67,7 +109,15 @@ func (s *SnitchClient) LookupTCPSocketProcess(srcPort uint16, dstAddr net.IP, ds
 	}
 	err = s.client.Call("ProcsnitchRPC.LookupTCPSocketProcess", tcpDescriptor, &info)
 	if err != nil {
-		log.Error("LookupTCPSocketProcess received a nil Info struct")
+		err = s.Dial()
+		if err != nil {
+			log.Errorf("LookupTCPSocketProcess received a nil Info struct: %s", err)
+		} else {
+			err = s.client.Call("ProcsnitchRPC.LookupTCPSocketProcess", tcpDescriptor, &info)
+			if err != nil {
+				log.Errorf("LookupTCPSocketProcess received a nil Info struct: %s", err)
+			}
+		}
 	}
 	return &info
 }
@@ -77,7 +127,15 @@ func (s *SnitchClient) LookupUDPSocketProcess(srcPort uint16) *procsnitch.Info {
 	info := procsnitch.Info{}
 	err = s.client.Call("ProcsnitchRPC.LookupUDPSocketProcess", srcPort, &info)
 	if err != nil {
-		log.Error("LookupUDPSocketProcess received a nil Info struct")
+		err = s.Dial()
+		if err != nil {
+			log.Errorf("LookupUDPSocketProcess received a nil Info struct: %s", err)
+		} else {
+			err = s.client.Call("ProcsnitchRPC.LookupUDPSocketProcess", srcPort, &info)
+			if err != nil {
+				log.Errorf("LookupUDPSocketProcess received a nil Info struct: %s", err)
+			}
+		}
 	}
 	return &info
 }
